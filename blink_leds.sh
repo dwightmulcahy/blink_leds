351b7eaaa1b33a60bfa6e8573f875443c2969d21
dwightmulcahy <dwightmulcahy@gmail.com>
2025-05-16 14:05:25 -0500

    updated directories to legacy and lastest kernal along w/ refactoring code

--- blink_leds.sh
+++ blink_leds.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 # based on this article: https://opensource.com/article/20/6/find-raspberry-pi
-
-set -o errexit
-set -o nounset
-
-# trap CTRL-C and kill
-trap quit INT TERM
+# updated with info on latest kernel: https://forums.raspberrypi.com/viewtopic.php?t=352416
 
 # make sure that this is being run as root
 if ! [ $(id -u) = 0 ]; then
@@ -14,37 +9,13 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-# set up all the directory constants
-LED0="/sys/class/leds/led0"
-LED1="/sys/class/leds/led1"
-LED0_TRIGGER="${LED0}/trigger"
-LED1_TRIGGER="${LED1}/trigger"
-LED0_BRIGHTNESS="${LED0}/brightness"
-LED1_BRIGHTNESS="${LED1}/brightness"
-ON=1
-OFF=0
+echo "Raspberry Pi LED Blinker"
 
-# make sure the directories exist
-if [ ! -d "$LED0" ]
-then
-  echo "Could not find an LED at ${LED0}"
-  echo "Perhaps try '/sys/class/leds/ACT'?"
-  exit 1
-fi
-if [ ! -d "$LED1" ]
-then
-  echo "Could not find an LED at ${LED1}"
-  echo "Perhaps try '/sys/class/leds/ACT'?"
-  exit 1
-fi
+set -o errexit
+set -o nounset
 
-# get the current trigger and brightness settings for the LEDs
-ENABLED_LED0=$(cat "${LED0_TRIGGER}" | sed -n -r 's/.*\[(.*)\].*/\1/p')
-BRIGHTNESS_LED0=$(cat "${LED0_BRIGHTNESS}")
-ENABLED_LED1=$(cat "${LED1_TRIGGER}" | sed -n -r 's/.*\[(.*)\].*/\1/p')
-BRIGHTNESS_LED1=$(cat "${LED1_BRIGHTNESS}")
-
-# trap will call this to reset all the LED states
+# trap CTRL-C and kill to reset all the LED states
+trap quit INT TERM
 function quit {
   echo ""
   echo "Setting led0 trigger back to [${ENABLED_LED0}]"
@@ -54,6 +25,46 @@ function quit {
   echo "${ENABLED_LED1}" >"${LED1_TRIGGER}"
   echo "${BRIGHTNESS_LED1}" >"${LED1_BRIGHTNESS}"
 }
+
+# set up the constants for turning the LEDs on/off
+ON=1
+OFF=0
+
+# the legacy (Buster) version of Raspberry Pi OS uses
+# led0 and led1 names whereas the recent kernel version
+# uses ACT and PWR for those names.
+#
+# make sure the directories exist for the LEDs
+LED0="/sys/class/leds/led0"
+if [ ! -d "$LED0" ]
+then
+  LED0="/sys/class/leds/ACT"
+fi
+
+LED1="/sys/class/leds/led1"
+if [ ! -d "$LED1" ]
+then
+  LED1="/sys/class/leds/PWR"
+fi
+
+# verify that we have valid directories for the LEDs
+if [ ! -d "$LED0" ] || [ ! -d "$LED1" ]
+then
+  echo "Unable to find directories for LEDs."
+fi
+
+# set up the trigger and brightness directory constants
+LED0_TRIGGER="${LED0}/trigger"
+LED1_TRIGGER="${LED1}/trigger"
+LED0_BRIGHTNESS="${LED0}/brightness"
+LED1_BRIGHTNESS="${LED1}/brightness"
+
+# get the current trigger and brightness settings for the LEDs
+ENABLED_LED0=$(cat "${LED0_TRIGGER}" | sed -n -r 's/.*\[(.*)\].*/\1/p')
+BRIGHTNESS_LED0=$(cat "${LED0_BRIGHTNESS}")
+ENABLED_LED1=$(cat "${LED1_TRIGGER}" | sed -n -r 's/.*\[(.*)\].*/\1/p')
+BRIGHTNESS_LED1=$(cat "${LED1_BRIGHTNESS}")
+
 
 echo -n "Blinking Raspberry Pi's LEDs - press CTRL-C to quit"
 
@@ -66,10 +77,13 @@ COUNT=0
 while true
 do
   let "COUNT=COUNT+1"
-  if [[ $COUNT -lt 30 ]]
+  if [[ $COUNT -lt 20 ]]
   then
     echo "${ON}" > "${LED0_BRIGHTNESS}"
     echo "${OFF}" > "${LED1_BRIGHTNESS}"
+    sleep 1
+    echo "${ON}" > "${LED0_BRIGHTNESS}"
+    echo "${ON}" > "${LED1_BRIGHTNESS}"
     sleep 1
     echo "${OFF}" > "${LED0_BRIGHTNESS}"
     echo "${ON}" > "${LED1_BRIGHTNESS}"
@@ -79,3 +93,4 @@ do
     break
   fi
 done
+
